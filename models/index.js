050633f13df004756b1897f6c57d51a962256cb1
Disastris <chavez.garcia.santi@gmail.com>
2023-11-26 17:46:12 -0600

    Fixed up Index.js

--- models/index.js
+++ models/index.js
@@ -1,8 +1,11 @@
 const User = require("./User");
-
+const Thought = require("./thought");
+const Reaction = require("./reaction");
 // Define relationships
 
 // Export
 module.exports = {
   User,
+  Thought,
+  Reaction,
 };
